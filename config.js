0758f1dfb366862ea5075e6c968d6b020dbe49de
dialejo24 <dialejosi530@gmail.com>
2025-05-12 00:02:42 -0500

    feat config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 export const PORT = 5000;
 export const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'Dads24052002@',
-    database: 'guarderia'
+    user: '',
+    password: '',
+    database: ''
 });
