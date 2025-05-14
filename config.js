ff4fd9dc45a1ed0783ebaefb3e23cd5ab4479b93
Raven0611 <jhobitiflo2000@gmail.com>
2025-05-13 19:13:24 -0500

    update
    update

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ import mysql from "mysql2";
 
 export const PORT = 5000;
 export const db = mysql.createConnection({
-    host: 'localhost',
-    user: '',
-    password: '',
-    database: ''
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "guarderia",
 });
